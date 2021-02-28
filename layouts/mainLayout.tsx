2861bb3300b3486bb4973b3f10146f44bfe5e32b
Anh Le <anh.ht.le@gmail.com>
2021-02-28 00:29:11 -0800

    lower case next head

--- layouts/mainLayout.tsx
+++ layouts/mainLayout.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Head from 'next/Head'
+import Head from 'next/head'
 import MainHeader from '../components/MainHeader'
 import MainFooter from '../components/MainFooter'
 
