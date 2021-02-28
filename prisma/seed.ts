4accb4f7842dba20d63501c71e754f9046972c04
Anh Le <anh.ht.le@gmail.com>
2021-02-27 21:10:55 -0800

    starter db

---
+++ prisma/seed.ts
@@ -0,0 +1,31 @@
+const { PrismaClient } = require('@prisma/client');
+let questions = require('./questions.json');
+
+const prisma = new PrismaClient();
+
+async function main() {
+    for (let question of questions) {
+        const q = await prisma.question.create({
+            data: {
+                name: question.name
+            }
+        });
+        
+        for (let answer of question.answers) {
+            await prisma.answer.create({
+                data: {
+                    name: answer.name,
+                    points: answer.points,
+                    questionId: q.id
+                }
+            })
+        }
+    }
+}
+
+main().catch(e => {
+    console.log(e);
+    process.exit(1)
+}).finally(() => {
+    prisma.$disconnect();
+})
